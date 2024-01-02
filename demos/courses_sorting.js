c4f8a02b32f6371f364238f16638098b5f8c0d10
bor564 <147197237+bor564@users.noreply.github.com>
2024-01-02 12:11:43 -0500

    reg-update

---
+++ demos/courses_sorting.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let courses = []
+
+courses.sort((courseA, courseB)=>{
+    if (courseA.Title) {
+        
+    }
+})
---
+++ demos/reuth-falsy.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let value = undefined;
+
+value = "";
+
+if (value=="" ||value ==undefined || value==null) {
+console.log(true);    
+}
+
+if (value){
+    console.log(true);
+}
